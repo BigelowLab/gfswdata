0fc4bad32f1a0677d9a7183ae7b407228040ab7f
Ben Tupper <btupper@bigelow.org>
2024-04-02 08:52:34 -0400

    add fleet stuff

--- R/fleet.R
+++ R/fleet.R
@@ -1,21 +1,6 @@
-#' Generate a template stars object
+#' Read a single fleet daily CSV file.
 #' 
-#' @export
-#' @param bb 4 element named numeric or bbox-class
-#' @param dx num, grid cell size in x
-#' @param dy num, grid cell size in y
-#' @param crs CRS projection
-#' @return stars object
-raster_template = function(bb = c(xmin = -180, xmax = 180, yim = -90, ymax = 90),
-                         dx = 0.1, dy = 0.1,
-                         crs = 4326){
-  if (!inherits(bb, "bbox")) bb = sf::st_bbox(bb, crs = crs)
-  stars::st_as_stars(bb, dx = dx, dy = dx)
-}
-
-#' Read a single mmsi daily CSV file.
-#' 
-#' mmsi-daily-csvs-10-v2-2020
+#' fleet-daily-csvs-100-v2-2020
 #' 
 #' @export
 #' @param date Date, POSIXt or (character as YYYY-mm-dd) to read
@@ -23,12 +8,22 @@ raster_template = function(bb = c(xmin = -180, xmax = 180, yim = -90, ymax = 90)
 #' @param form char, one of 'raw', 'sf' or 'stars'
 #' @return table (tibble), sf POINT or stars object depending upon \code{form}
 read_mmsi_daily = function(date = "2020-01-01",
-                           path = gfw_path("mmsi-daily-csvs-10-v2-2020"),
+                           path = gfw_path("fleet-daily-csvs-100-v2-2020"),
                            form = c('raw', 'sf', 'stars')[1]){
   if (!inherits(date, "character")) date  = format(date, "%Y-%m-%d")
   filename = file.path(path, paste0(date, ".csv"))
   
-  x = readr::read_csv(filename, col_types = "Dnnnnn")
+  x = readr::read_csv(filename, 
+                      col_types = readr::cols(
+                        date = readr::col_date(format = ""),
+                        cell_ll_lat = readr::col_double(),
+                        cell_ll_lon = readr::col_double(),
+                        flag = readr::col_character(),
+                        geartype = readr::col_character(),
+                        hours = readr::col_double(),
+                        fishing_hours = readr::col_double(),
+                        mmsi_present = readr::col_double()
+                      ))
   
   if (tolower(form[1]) %in% c("sf", "stars")){
     x = sf::st_as_sf(x, 
